ae4b7ea519ddb2ec0872588afbf64e78143d114f
adarsh landge <adarsh.landge10604@gmail.com>
2024-09-30 21:21:30 +0530

    create pages and routes

---
+++ Backend/migrations/20240923112804_init.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('profile',(table)=>{
+        table.increments("id").primary()
+        table.string("name").notNullable()
+            
+        
+        
+    })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('profile')
+  
+};
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,20 @@
-import Landing from './pages/Landing';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './About';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-  <Landing/>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/profile' element={<Profile/>} />
+    </Routes>
+  </BrowserRouter>
   )
 }
