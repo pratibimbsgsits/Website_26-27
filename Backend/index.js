c872220244cd76c5854344ddfd731ac06990fa2b
adarsh landge <adarsh.landge10604@gmail.com>
2024-10-08 22:06:06 +0530

    a new backend with mongoDB and database connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const passport = require("passport");
-const db = require("./db/index.js");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(passport.initialize());
-
-
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
   });
-  
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000!");
+});
